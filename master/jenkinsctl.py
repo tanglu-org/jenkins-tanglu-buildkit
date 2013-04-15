45f6ca8c13f6d63f42262ab8a0ea9d374f76b67a
Matthias Klumpp <matthias@tenstral.net>
2013-04-16 01:16:19 +0200

    Add double linebreak to the end

--- master/jenkinsctl.py
+++ master/jenkinsctl.py
@@ -72,7 +72,7 @@ class JenkinsBridge:
         return p.returncode, resLines
 
     def _createJobTemplate(self, pkgname, pkgversion, component, distro, architecture, info=""):
-        info_html = info.replace("<", "&lt;").replace(">", "&gt;").replace("\n", "<br/>")
+        info_html = info.replace("<", "&lt;").replace(">", "&gt;").replace("\n", "<br/>") + "<br/><br/>"
         jobStr = self._jobTemplateStr.replace("{{architecture}}", architecture)
         jobStr = jobStr.replace("{{distroname}}", escape(distro))
         jobStr = jobStr.replace("{{component}}", component)
