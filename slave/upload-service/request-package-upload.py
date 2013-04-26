0cbc6fd2f168d2467b98cfb1ee214dcddbb44c22
Matthias Klumpp <matthias@tenstral.net>
2013-04-26 23:59:21 +0200

    trivial: Add missing import

--- slave/upload-service/request-package-upload.py
+++ slave/upload-service/request-package-upload.py
@@ -18,6 +18,8 @@
 
 import dbus
 import sys
+import os
+import subprocess
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -25,6 +27,11 @@ if __name__ == "__main__":
         exit(1)
     u_location = sys.argv[1]
     u_changesfile = sys.argv[2]
+
+    # ensure we have a session bus
+    proc = subprocess.Popen([os.path.dirname(os.path.realpath(__file__)) + "/run_dbus_session.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.wait()
+
     print (u_location + " ## " + u_changesfile)
     bus = dbus.SessionBus()
     uploadservice = bus.get_object('org.debian.PackageUpload', '/org/debian/packageupload')
