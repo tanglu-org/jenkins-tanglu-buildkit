b791859726d0e751b967f0df14bfec6a3f938b48
Matthias Klumpp <matthias@tenstral.net>
2013-05-16 02:54:58 +0200

    Only check for changed build state every 3h

---
+++ master/extra/delete-failed-jobs.groovy
@@ -0,0 +1,12 @@
+hudsonInstance = hudson.model.Hudson.instance
+
+allItems = hudsonInstance.items
+pkgJobs = allItems.findAll{job -> job.name.contains("pkg+")}
+
+for (job in pkgJobs) {
+  build = job.getLastBuild()
+    if (build != null) {
+      if (build.result != Result.SUCCESS)
+        job.delete()
+    }
+}
