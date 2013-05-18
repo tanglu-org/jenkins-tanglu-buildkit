76a8b191bda3722ac4d5213ae3c30a26638624fd
Matthias Klumpp <matthias@tenstral.net>
2013-05-19 01:19:17 +0200

    Add data to needsbuild list when updating builder index

--- master/maintain-jenkins-jobs.py
+++ master/maintain-jenkins-jobs.py
@@ -58,6 +58,8 @@ class BuildJobUpdater:
             pkgNameList.append(pkg.pkgname)
         self._jenkins.set_registered_packages_list(pkgNameList)
 
+        needsbuild_list = open("/srv/dak/queue/needsbuild.list", "w")
+
         for pkg in pkgList:
             archs = self._filterUnsupportedArchs(pkg.archs)
 
@@ -88,11 +90,13 @@ class BuildJobUpdater:
                 if self.debugMode:
                         print("INFO: Skipping %s, package not created/updated (higher version available?)" % (pkg.pkgname))
 
-         #           if not arch in pkg.installedArchs:
-         #               if self.debugMode:
-         #                   print("Package %s not built for %s!" % (pkg.pkgname, arch))
-         #               if self.scheduleBuilds:
-         #                   self._jenkins.schedule_build_if_not_failed(pkg.pkgname, pkg.version, arch)
+            for (arch in pkgArchs):
+                if not arch in pkg.installedArchs:
+                    if self.debugMode:
+                        print("Package %s not built for %s!" % (pkg.pkgname, arch))
+                    if self.scheduleBuilds:
+                        needsbuild_list.write("%s_%s\n" % (pkg.pkgname, pkg.version))
+                        #self._jenkins.schedule_build_if_not_failed(pkg.pkgname, pkg.version, arch)
 
     def _get_cruft_jobs(self):
         pkgList = self._pkginfo.get_all_packages()
